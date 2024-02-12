22dc9a5151ec663143d69c3ea667bef0744597a0
Mohamed <mohamedmoustaphatr@gmail.com>
2024-02-12 14:52:28 +0100

    activite commit

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const { Admin } = require('../controllers/auth.controller');
 const { verifyToken } = require('../middlewares/auth.middleware');
@@ -17,14 +18,20 @@ router.post('/login', async (req, res) => {
             return res.status(500).json({ error: 'Internal Server Error' });
         }
         else {
-            const checkPassword = await bcrypt.compareSync(password, data[0].pass);
-
-            if (!checkPassword) {
+            const hashedEnteredPassword = crypto.createHash('md5').update(password).digest('hex');
+            const password_db = data[0].pass;
+            // const checkPassword = await bcrypt.compareSync(password, data[0].pass);
+            if (hashedEnteredPassword === password_db) {
+                // Les mots de passe correspondent
+                const token = jwt.sign({ userId: data[0].N, id_personnel: data[0].id_personnel }, process.env.SECRET_KEY);
+                res.status(200).json({ message: 'Login successful', access_token: token });
+            } else {
+                // Les mots de passe ne correspondent pas
                 return res.status(401).json({ message: 'Invalid credentials' });
             }
-
-            const token = jwt.sign({ userId: data[0].N, id_personnel: data[0].id_personnel }, process.env.SECRET_KEY);
-            res.status(200).json({ message: 'Login successful', access_token: token });
+            // if (!checkPassword) {
+            //     return res.status(401).json({ message: 'Invalid credentials' });
+            // }
         }
     })
 });
